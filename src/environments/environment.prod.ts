2b486a29e4bfb9de8e02b05c3f7d6170fc552bce
owezzy <owenadira@gmail.com>
2023-06-11 16:02:18 +0300

    feat(ec2 backend): aws ec2 docker api
    use api docker image running on an aws ec2 instance

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,10 @@
 export const environment = {
   production: true,
-  Deezer_API:'https://api.deezer.com',
-  Deezer_API_LOCAL:'http://localhost:3000/api',
+  Deezer_API: 'https://api.deezer.com',
+  Deezer_API_LOCAL: 'http://localhost:3000/api',
   HEROKU_API: 'https://dvt-deezer-proxy.herokuapp.com/api',
-  AWS_API_ENDPOINT: 'https://49v7d5eop3.execute-api.eu-west-2.amazonaws.com/default/dvtDeezerProxy-staging/api',
+  AWS_API_ENDPOINT:
+    'https://49v7d5eop3.execute-api.eu-west-2.amazonaws.com/default/dvtDeezerProxy-staging/api',
+  AWS_EC2_API: 'http://ec2-13-41-78-85.eu-west-2.compute.amazonaws.com:3000',
   ngrx_logs: false,
-
 };
